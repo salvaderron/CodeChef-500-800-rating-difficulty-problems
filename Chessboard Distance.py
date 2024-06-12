7b6de54136e8c3b42a942ab81ad6737d319405e9
Salvader Ron Nathaniel <78250990+salvaderron@users.noreply.github.com>
2024-06-12 18:28:34 +0530

    Update Chessboard Distance.py

--- Chessboard Distance.py
+++ Chessboard Distance.py
@@ -9,3 +9,14 @@ for i in range(0,n):
     if d2<0:
         d2=-1*d2
     print(max(d1,d2))
+
+# Can also be done using abs() function in python
+
+# cook your dish here
+t=int(input())
+for i in range(t):
+    x1,y1,x2,y2=map(int,input().split(" "))
+    if abs(x1-x2)>abs(y1-y2):
+        print(abs(x1-x2))
+    else:
+        print(abs(y1-y2))
